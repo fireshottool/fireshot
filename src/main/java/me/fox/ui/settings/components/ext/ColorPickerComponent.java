c101a4c66f15381626119930b28db1c0f18542d1
Fox Foxed <foxfoxed297@gmail.com>
2021-05-25 19:57:35 +0200

    Refactor ColorComponent to ColorPickerComponent

--- src/main/java/me/fox/ui/settings/components/ext/ColorPickerComponent.java
+++ src/main/java/me/fox/ui/settings/components/ext/ColorPickerComponent.java
@@ -1,4 +1,22 @@
-package me.fox.ui.components.settings.ext;
+/*
+ * Fireshotapp is an easy to use screenshot program
+ *     Copyright (C) 2021  Ausgefuchster
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package me.fox.ui.settings.components.ext;
 
 import lombok.Getter;
 import me.fox.Fireshotapp;
@@ -20,14 +38,14 @@ import java.awt.event.ActionListener;
 
 
 @Getter
-public class ColorComponent extends SettingsComponent {
+public class ColorPickerComponent extends SettingsComponent {
 
     private final FoldEdgeLabel foldEdgeLabel;
     private final JLabel label;
     private final JButton button;
     private final ColorPickerDialog colorPickerDialog;
 
-    public ColorComponent(String label, Point location, ActionListener colorChangedListener, JFrame frame) {
+    public ColorPickerComponent(String label, Point location, ActionListener colorChangedListener, JFrame frame) {
         super(location);
 
         this.foldEdgeLabel = new FoldEdgeLabel(Color.red.toString());
