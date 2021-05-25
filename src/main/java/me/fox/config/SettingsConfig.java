8b82a6db00dc696f7b295cc2fff0cdd4e4b173c1
Fox Foxed <foxfoxed297@gmail.com>
2021-05-25 22:01:19 +0200

    Add SettingsConfig

---
+++ src/main/java/me/fox/config/SettingsConfig.java
@@ -0,0 +1,36 @@
+/*
+ * Fireshotapp is an easy to use screenshot program
+ *     Copyright (C) 2021  Ausgefuchster
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package me.fox.config;
+
+import lombok.Data;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * @author (Ausgefuchster)
+ * @version (~ 14.05.2021)
+ */
+
+@Data
+public class SettingsConfig {
+
+    private Set<String> customColors = new LinkedHashSet<>();
+    private String lastPane = "Appearance";
+}
